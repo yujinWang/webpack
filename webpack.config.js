c87465ced5f5d0a4ae5777e2b6181ef08558e8e9
yujinWang <1219044051@qq.com>
2019-06-19 00:19:45 +0800

    插件初体验

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 //采用common.js语法，也就是node.js语法
 const path = require("path");
+//引入html-webpack-plugin插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 //因为webpack这个文件使用的是node.js的语法，所以不能使用export defaule的es6语法
 module.exports = {
   entry: "./src/main.js", //webapck解析入口文件 //webpack打包之后文件输出路径
   output: {
     path: path.resolve("__dirname", "dist"), //这里的输出路径需要使用绝对路径
     filename: "main.js"
-  }
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }
