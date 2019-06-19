9c45700b4a8b129bb573e65267368b9b163c2f0b
yujinWang <1219044051@qq.com>
2019-06-19 23:23:14 +0800

    loader

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 //webpack入口文件
-
-import a from "./a";
-import b from "./b";
-import c from "./c";
-
-a();
-b();
-c();
+import React from "react";
+import ReactDom from "react-dom";
+//render函数接受两个参数，第一个是要添加的元素，第二个是要添加到的那个dom节点
+ReactDom.render( <
+  div > React < /div>,
+  document.getElementById("root")
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 //采用common.js语法，也就是node.js语法
 const path = require("path");
-//引入html-webpack-plugin插件
+//引入html-webpack-plugin插件，打包之后会在dist目录下面自动生成一个index.html文件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 //因为webpack这个文件使用的是node.js的语法，所以不能使用export defaule的es6语法
 module.exports = {
   entry: "./src/main.js", //webapck解析入口文件 //webpack打包之后文件输出路径
   output: {
     path: path.resolve("__dirname", "dist"), //这里的输出路径需要使用绝对路径
-    filename: "main.js"
+    filename: "main.js" //输出的打包文件名称
   },
-  mode: "development",
+  mode: "development", //webpack4以上的版本需要配置此对象
   plugins: [
     new HtmlWebpackPlugin()
-  ]
+  ],
+  module: {
+    //匹配规则，寻找js文件，并使用use使用对应的loader来解析此js文件
+    //具体是babel-loader以及对应的库babel-core
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: "babel-loader",
+        options: {
+          presets: ["react"]
+        }
+      }]
+    }]
+  }
 }
